cdddb4be5b1ab8fd8c0d54369f38fbc384491cb9
Princeyyyy <princemumo80@gmail.com>
2021-06-25 11:34:02 +0300

    finished user registration and login

--- app/src/main/java/com/example/budgeting_app/LoginActivity.java
+++ app/src/main/java/com/example/budgeting_app/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.budgeting_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,16 +32,59 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.gotoRegister)
     TextView gotoRegister;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
         gotoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onRegistrationClick(v);
+            }
+        });
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailString = logEmail.getText().toString();
+                String passwordString = logPassword.getText().toString();
+
+                if (TextUtils.isEmpty(emailString)) {
+                    logEmail.setError("Email is Required");
+                }
+
+                if (TextUtils.isEmpty(passwordString)) {
+                    logPassword.setError("Password is Required");
+                } else {
+
+                    progressDialog.setMessage("Login in Progress");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                progressDialog.dismiss();
+                            } else {
+                                Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/budgeting_app/RegistrationActivity.java
+++ app/src/main/java/com/example/budgeting_app/RegistrationActivity.java
@@ -1,13 +1,22 @@
 package com.example.budgeting_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,16 +32,58 @@ public class RegistrationActivity extends AppCompatActivity {
     @BindView(R.id.gotoLogin)
     TextView gotoLogin;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
         gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onLoginClick(v);
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = regEmail.getText().toString();
+                String password = regPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    regEmail.setError("Email is Required");
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    regPassword.setError("Password is Required");
+                }else {
+
+                    progressDialog.setMessage("Registration in Progress");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                progressDialog.dismiss();
+                            } else {
+                                Toast.makeText(RegistrationActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
