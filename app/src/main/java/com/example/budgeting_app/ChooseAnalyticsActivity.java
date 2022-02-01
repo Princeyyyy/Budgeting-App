0a5f5d66f7bb07f986c7582d85899ae717ef34f8
Princeyyyy <princemumo80@gmail.com>
2022-02-01 16:50:23 +0300

    finished month and week analytic ui

--- app/src/main/java/com/example/budgeting_app/ChooseAnalyticsActivity.java
+++ app/src/main/java/com/example/budgeting_app/ChooseAnalyticsActivity.java
@@ -1,14 +1,32 @@
 package com.example.budgeting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ChooseAnalyticsActivity extends AppCompatActivity {
+
+    @BindView(R.id.analyticsTodayCardView)
+    CardView analyticsTodayCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_analytics);
+        ButterKnife.bind(this);
+
+        analyticsTodayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseAnalyticsActivity.this, DailyAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
